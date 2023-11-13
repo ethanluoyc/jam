22c967cbe6bae77d372d65020ced4a5756d4472b
Yicheng Luo <ethanluoyc@gmail.com>
2023-11-13 14:00:26 +0000

    Remove old test

--- tests/unit/nfnet_flax_test.py
+++ tests/unit/nfnet_flax_test.py
@@ -1,7 +1,6 @@
 # type: ignore
 from absl.testing import absltest
 import chex
-import flax
 import jax
 import jax.numpy as jnp
 
@@ -47,43 +46,6 @@ class FlaxNFNetTest(absltest.TestCase):
             variables, inputs, is_training=True, rngs={"dropout": jax.random.PRNGKey(0)}
         )
         chex.assert_shape(outputs["logits"], (4, 10))
-
-    def test_nfnet_decay_params(self):
-        net = nfnet.NFNet(10, variant="F0")
-        inputs = jnp.ones((1, 224, 224, 3))
-        params = net.init(jax.random.PRNGKey(0), inputs, is_training=False)["params"]
-
-        def decay_mask_fn(params):
-
-            def decay_fn(path):
-                if path[-1] == "skipgain":
-                    return False
-                if path[-1] == "gain":
-                    return False
-                if "conv" in path[-2] and "bias" in path[-1]:
-                    return False
-                return True
-
-            flat_params = flax.traverse_util.flatten_dict(params)
-            # True for params that we want to apply weight decay to
-            flat_mask = {path: decay_fn(path) for path in flat_params}
-            return flax.traverse_util.unflatten_dict(flat_mask)
-
-        def clip_mask_fn(params):
-            flat_params = flax.traverse_util.flatten_dict(params)
-            # True for params that we want to apply weight decay to
-            flat_mask = {path: path[0] == 'linear' for path in flat_params}
-            return flax.traverse_util.unflatten_dict(flat_mask)
-
-        decay_mask = decay_mask_fn(params)
-        clip_mask = clip_mask_fn(params)
-        print("Decay mask")
-        for k, v in flax.traverse_util.flatten_dict(decay_mask, sep="/").items():
-            print(k, v)
-
-        # print("Clip mask")
-        # for k, v in flax.traverse_util.flatten_dict(clip_mask, sep="/").items():
-        #     print(k, v)
 
 
 if __name__ == "__main__":
